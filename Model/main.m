64d91b2a3f96f32a6f5f9f324e4053372d9b6e56
DeriZSY <derizsy@foxmail.com>
2017-12-11 22:12:07 +0800

    Main program of mathematical modeling

---
+++ Model/main.m
@@ -0,0 +1,16 @@
+% V_max : 17mm bullet : 30/ 45mm bullet : 16.5;
+global V_max = 30;
+% HP : infantry : 750,1000,1500;
+% Sentinal : 3000
+% Hero : 1500/2500/3500
+global HP_max = 750;
+% Q0 : infantry : 1600,3000,6000;
+% Sentinal 4500
+% Hero, 17mm : 1600/3000/6000, 45mm : 3200/6400/12800
+global Q0 = 1600;
+% colling rate : infantry : 500/1000/2000
+% Sentinal : 1500
+% Hero : 17mm : 500/1000/2000; 45mm : 1600/3200/6400
+global cooling_rate = 500;
+global fc = 10;
+global AD = 50; % 17mm: 50, 45mm : 500
