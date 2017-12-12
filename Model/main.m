9ad84a9fe794c36a2e271416879fbaa0a42bb21d
DeriZSY <derizsy@foxmail.com>
2017-12-12 10:34:53 +0800

    Suggested Objective and Constraint

--- Model/main.m
+++ Model/main.m
@@ -1,5 +1,5 @@
 % V_max : 17mm bullet : 30/ 45mm bullet : 16.5;
-global V_max = 30;
+global V0 = 30;
 % HP : infantry : 750,1000,1500;
 % Sentinal : 3000
 % Hero : 1500/2500/3500
@@ -12,5 +12,21 @@ global Q0 = 1600;
 % Sentinal : 1500
 % Hero : 17mm : 500/1000/2000; 45mm : 1600/3200/6400
 global cooling_rate = 500;
-global fc = 10;
+global fc = 10; % refresh frequency
 global AD = 50; % 17mm: 50, 45mm : 500
+
+% Occasion 1: Find max possible shooting velocity
+
+% Objective function:
+f1 = hit(hit_rate,AD,V1) % maximize
+f2 = shooting_range(V1) % maximize
+% Neat Damage
+HP_lose = heating(fc,V0,V1);
+f3 = f1 - HP_lose;
+% Penalty :
+% 1. Stabel & Suggested Shooting Velocity
+
+% Constraint :
+UB = [30]
+LB = [0]
+HP_lose = heating(fc,V0,V1); % HP_lose > 0
